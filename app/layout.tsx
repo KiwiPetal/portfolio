04e7b0582b54e7d46bc53ac0ed86cb1e7c28ffe4
KiwiPetal <kiwipetal@kittymail.com>
2024-03-05 17:40:53 +0100

    Small fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: "Kaylee's portfolio",
-  description: "TBD",
+  description: "TBD"
 }
 export default function RootLayout({
   children,
