bec2e9468ce0fd6bd2a6cb474b49eda1305b6e03
KiwiPetal <kiwipetal@kittymail.com>
2024-03-05 18:25:08 +0100

    Added openGraph metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,12 @@ import type { Metadata } from 'next'
 export const metadata: Metadata = {
   title: "Kaylee's portfolio",
   description: "A freelance coder, weavin' magic in the digital realm.",
+  openGraph: {
+    type: "website",
+    images: [{
+      url: "https://little-kiwi.net/icon.png"
+    }]
+  }
 }
 export default function RootLayout({
   children,
