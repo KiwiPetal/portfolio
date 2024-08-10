e7240a008b450d3fd6dedb5a5fbb43d00cfe140e
Kaylee <kiwipetal@kittymail.com>
2024-08-10 13:24:55 +0200

    Turn className optional

--- app/components/TitleChanger/Title.tsx
+++ app/components/TitleChanger/Title.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 
 interface vars {
   titles: string[];
-  className: string;
+  className?: string;
 }
 
 const TitleChanger = ({ titles, className }: vars) => {
