eed5c9ef66fcd68169eb0916abf1b7a4c3a9b06c
Kiwi <kiwipetal@kittymail.com>
2024-11-02 22:51:15 +0100

    feat: Add multiple picture scroll

--- app/components/Gallery/Card/Card.tsx
+++ app/components/Gallery/Card/Card.tsx
@@ -12,13 +12,37 @@ interface props {
   link?: string;
 }
 
+// Framer motion {{{
 const openSpring = { type: "spring", stiffness: 200, damping: 30 };
 const closeSpring = { type: "spring", stiffness: 300, damping: 35 };
-
+const pipVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 1,
+      ease: [0.65, 0.05, 0.36, 1],
+    },
+  }
+}
+const navVariants = {
+  hidden: {
+    opacity: 0,
+    display: "none"
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 1.7,
+      ease: [0.65, 0.05, 0.36, 1],
+    },
+  }
+}
 const infoVariants = {
   open: {
     height: "auto",
-    paddingTop: "15px",
     paddingBottom: "15px",
     duration: 0.3,
     transition: {
@@ -29,7 +53,6 @@ const infoVariants = {
   },
   close: {
     height: "0px",
-    paddingTop: "0px",
     paddingBottom: "0px",
     transition: {
       duration: 0.8,
@@ -38,10 +61,13 @@ const infoVariants = {
     },
   }
 }
+// }}}
 
 export function Card(props: props) {
   const [open, setOpen] = useState(false);
   const [currentImage, setCurrentImage] = useState(0)
+
+  const length = props.pics.length;
   return (
     <div className={styles.card_wrapper}>
       <Overlay open={open} setClose={() => setOpen(false)} />
@@ -56,8 +82,31 @@ export function Card(props: props) {
         className={
           `${styles.card} ${open ? styles.open : ""}`
         }>
-        <img className={styles.mainImg} onClick={() => setOpen(!open)} alt={props.title ? props.title : ""} src={props.pics[currentImage]} />
+        <motion.img className={styles.mainImg} layoutId={"img_" + props.id} onClick={() => setOpen(!open)} alt={props.title ? props.title : ""} src={props.pics[currentImage]} />
         <motion.div variants={infoVariants} animate={open ? "open" : "close"} className={styles.description}>
+          {
+            length > 1 && (
+              <motion.div
+                variants={pipVariants}
+                animate={open ? "visible" : "hidden"}
+                className={styles.pipsWrapper}>
+                <div className={styles.pips}>
+                  {props.pics.map((_, i) => {
+                    return (
+                      <div
+                        key={"pip" + i}
+                        onClick={() => setCurrentImage(i)}
+                        className={`${styles.pip}`} />
+                    )
+                  })}
+                  <div
+                    className={`${styles.pip} ${styles.activePip}`}
+                    style={{ '--length': length, '--current': currentImage } as React.CSSProperties}
+                  />
+                </div>
+              </motion.div>
+            )
+          }
           <p className={styles.title}>
             {props.title}
           </p>
